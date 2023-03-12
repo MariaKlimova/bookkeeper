05611ce5da24f26db842dc73856d337c38622e84
Maria Klimova <masha-klimova@inbox.ru>
2023-03-12 20:50:46 +0300

    finished budget model and budget tests

--- tests/test_models/test_budget.py
+++ tests/test_models/test_budget.py
@@ -1,1 +1,29 @@
 import pytest
+
+from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.models.budget import Budget
+
+
+@pytest.fixture
+def repo():
+    return MemoryRepository()
+
+
+def test_create_with_full_args_list():
+    b = Budget(period='day', limit=100, category=1, pk=1)
+    assert b.period == 'day'
+    assert b.limit == 100
+    assert b.category == 1
+
+
+def test_create_with_no_category():
+    b = Budget(period='week', limit=1000, pk=1)
+    assert b.period == 'week'
+    assert b.limit == 1000
+    assert b.category == None
+
+
+def test_can_add_to_repo(repo):
+    b = Budget(period='week', limit=1000)
+    pk = repo.add(b)
+    assert b.pk == pk
