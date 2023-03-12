ed0adfedb15c88e9e191b454ec7e43fe4a01f80d
Maria Klimova <masha-klimova@inbox.ru>
2023-03-12 17:28:29 +0300

    code style fixes

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -1,9 +1,11 @@
 """Модуль описывает репозиторий, работающий с базой данных"""
-
-from bookkeeper.repository.abstract_repository import AbstractRepository, T
-from inspect import get_annotations
-import sqlite3
 from typing import Any
+import sqlite3
+from inspect import get_annotations
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
+
+
+
 
 TYPE_DESCRIPTIONS = {
     "str": "TEXT NOT NULL",
@@ -20,7 +22,9 @@ class SQLiteRepository(AbstractRepository[T]):
         self.table_name = cls.__name__.lower()
         self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop("pk")
-        descriptions = [f'{field} {TYPE_DESCRIPTIONS.get(self.fields[field].__name__)}' for field in self.fields]
+        descriptions = \
+            [f'{field} {TYPE_DESCRIPTIONS.get(self.fields[field].__name__)}'
+             for field in self.fields]
         create_query = f'CREATE TABLE IF NOT EXISTS {self.table_name}' \
                        f' ({"pk INTEGER NOT NULL PRIMARY KEY,"+ ",".join(descriptions)});'
         with sqlite3.connect(self.db_file) as con:
@@ -30,13 +34,13 @@ class SQLiteRepository(AbstractRepository[T]):
 
     def add(self, obj: T) -> int:
         names = ', '.join(self.fields.keys())
-        p = ', '.join("?"*len(self.fields))
+        places = ', '.join("?"*len(self.fields))
         values = [getattr(obj, x) for x in self.fields]
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
             cur.execute('PRAGMA foreign_keys = ON')
             #s = f'INSERT INTO {self.table_name} ({names}) VALUES ({p})'
-            cur.execute(f'INSERT INTO {self.table_name} ({names}) VALUES ({p})', values)
+            cur.execute(f'INSERT INTO {self.table_name} ({names}) VALUES ({places})', values)
             obj.pk = cur.lastrowid
         con.close()
         return obj.pk
@@ -49,6 +53,7 @@ class SQLiteRepository(AbstractRepository[T]):
             if len(record) > 0:
                 return record[0]
         con.close()
+        return None
     def update(self, obj: T) -> None:
         names = self.fields.keys()
         #p = ', '.join("?" * len(self.fields))
